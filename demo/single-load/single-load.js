27d8b598b8176049a784a984918d5f39b8b1605d
HuiminLiu <hihuimin@gmail.com>
2017-01-09 17:23:04 +0800

    Tab > Spaces

--- demo/single-load/single-load.js
+++ demo/single-load/single-load.js
@@ -11,28 +11,28 @@ const imgUrlThumbnail = 'http://storage.360buyimg.com/mtd/home/lion1483683731203
 const imgUrlOriginal = 'http://storage.360buyimg.com/mtd/home/lion1483624894660.jpg'
 
 Page({
-	data: {
-		msg: '',
-		imgUrl: ''
-	},
-	onLoad() {
-		//初始化图片预加载组件
-		this.imgLoader = new ImgLoader(this)
-	},
-	loadImage() {
-		//加载缩略图
-		this.setData({
-			msg: '大图正在拼命加载..',
-			imgUrl: imgUrlThumbnail
-		})
+    data: {
+        msg: '',
+        imgUrl: ''
+    },
+    onLoad() {
+        //初始化图片预加载组件
+        this.imgLoader = new ImgLoader(this)
+    },
+    loadImage() {
+        //加载缩略图
+        this.setData({
+            msg: '大图正在拼命加载..',
+            imgUrl: imgUrlThumbnail
+        })
 
-		//同时对原图进行预加载，加载成功后再替换
-		this.imgLoader.load(imgUrlOriginal, (err, data) => {
-			console.log('图片加载完成', err, data.src)
-			this.setData({ msg: '大图加载完成~' })
+        //同时对原图进行预加载，加载成功后再替换
+        this.imgLoader.load(imgUrlOriginal, (err, data) => {
+            console.log('图片加载完成', err, data.src)
+            this.setData({ msg: '大图加载完成~' })
 
-			if (!err)
-				this.setData({ imgUrl: data.src })
-		})
-	}
+            if (!err)
+                this.setData({ imgUrl: data.src })
+        })
+    }
 })
